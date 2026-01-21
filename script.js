ea08225939fb33be9f178abe0ac76a9460bd9ec5
moheeel <mohamed.el-azrak@utbm.fr>
2026-01-21 16:00:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "En espérant que ça te plaise !!!! 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
